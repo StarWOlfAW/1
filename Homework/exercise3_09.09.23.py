db4955c0a7b434538bfc3cda3cf9e4810fd4f79e
StarWolfAW <evdryakin@gmail.com>
2023-09-14 15:17:38 +0300

    hm1.1

---
+++ Homework/exercise3_09.09.23.py
@@ -0,0 +1,8 @@
+from math import sqrt #Это мне предложил сам VS code. Похоже что это импорт команд из библиотеки?????
+
+
+x = float(input("x ="))
+y = float(input("y ="))
+z = float(input("z ="))
+v = sqrt(x * x + y * y + z * z) 
+print("Длина вектора равна ", v)
--- Homework/exercise5_09.09.23.py
+++ Homework/exercise5_09.09.23.py
@@ -1,14 +1,16 @@
-print ("Введите x"); x=float(input())
-print ("Введите y"); y=float(input())
+x = float(input("Введите x "))
+y = float(input("Введите y "))
 if (x > 0 and y > 0) :
      print("Точка в 1 четверти")
 elif (x < 0 and y>0) :
      print("Точка в 2 четверти")
-elif (x<0 and y<0) : 
+elif (x < 0 and y <0 ) : 
      print("Точка в 3 четверти")
-elif (x>0 and y<0) :
+elif (x > 0 and y < 0) :
      print("Точка в 4 четверти")
-elif (x==0 and y!=0) :
+elif (x == 0 and y != 0) :
      print("Точка на оси Y")
-elif (x!=0 and y==0) :
+elif (x != 0  and y == 0) :
      print("Точка на оси X")
+elif (x == 0 and y == 0) :
+     print("Точка в начале системы кооржинат")
