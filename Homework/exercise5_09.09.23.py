cc7b7dda42cfb736fa86930053afdc0d72f5a7aa
mo1odoy4 <143870378+mo1odoy4@users.noreply.github.com>
2023-09-23 10:42:22 +0300

    HM1

--- Homework/exercise5_09.09.23.py
+++ Homework/exercise5_09.09.23.py
@@ -1,16 +1,16 @@
 x = float(input("Введите x "))
 y = float(input("Введите y "))
-if (x > 0 and y > 0) :
+if x > 0 and y > 0:
      print("Точка в 1 четверти")
-elif (x < 0 and y>0) :
+elif x < 0 and y > 0:
      print("Точка в 2 четверти")
-elif (x < 0 and y <0 ) : 
+elif x < 0 and y < 0: 
      print("Точка в 3 четверти")
-elif (x > 0 and y < 0) :
+elif x > 0 and y < 0:
      print("Точка в 4 четверти")
-elif (x == 0 and y != 0) :
+elif x == 0 and y != 0:
      print("Точка на оси Y")
-elif (x != 0  and y == 0) :
+elif x != 0  and y == 0:
      print("Точка на оси X")
-elif (x == 0 and y == 0) :
+else:
      print("Точка в начале системы кооржинат")
