49e1cbcae2f57508f75e24a6e15421f981e534c1
mo1odoy4 <143870378+mo1odoy4@users.noreply.github.com>
2023-10-14 14:05:18 +0300

    edit

--- Homework3/exercise4_30.09.23.py
+++ Homework3/exercise4_30.09.23.py
@@ -1,10 +1,8 @@
+from collections import Counter
 from ex1 import thv
 
 things = thv(input())
-que = {}
-
-for th in things:
-    que[th] = que.get(th, 0) + 1
+que = Counter(things)
 
 print("Элемент | Частота")
 print(*[f"{x} | {que[x]}" for x in que], sep="\n")
