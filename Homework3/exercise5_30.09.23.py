bf7807ed75e4a38dd224c09611d498ca846849d5
Evgeniy <143869955+StarWOlfAW@users.noreply.github.com>
2023-10-14 10:50:02 +0300

    Update exercise5_30.09.23.py

--- Homework3/exercise5_30.09.23.py
+++ Homework3/exercise5_30.09.23.py
@@ -3,6 +3,7 @@ from ex1 import thv
 def mid(*num) -> float:   #каждое число  к float
     return sum(num) / len(num)
 
+
 print(mid(*map(int, thv(input()))))
 # map выступает вместо for 
 # Instead of using a for loop, the map() function provides a way of applying a function to every item in an iterable.
