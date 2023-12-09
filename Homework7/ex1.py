12a687e3f6b5906328e19861d9eec07046ddf8f3
sofiashedova <sofia.shedova@gmail.com>
2023-12-09 11:21:01 +0300

    upload

--- Homework7/ex1.py
+++ Homework7/ex1.py
@@ -2,16 +2,17 @@ from collections import deque
 
 
 def search(gr, st, cur):
+    if not gr:
+        return None
     k = deque([(st, 0)])
-    d = set([st])  
-
+    d = set([st])
     while k:
         this, forward = k.popleft()
         if this == cur:
             return forward
         for near in gr.get(this, []):
             if near not in d:
-                k.append(near, forward + 1)
+                k.append((near, forward + 1))
                 d.add(near)
-    
-    return
+
+    return None
--- Homework7/tests/test_1.py
+++ Homework7/tests/test_1.py
@@ -5,9 +5,10 @@ from Homework7.ex1 import search
 @pytest.mark.parametrize(
     ('gr', 'st', 'cur', 'forward'),
     [
-        ({'A':[],'B':[]'C':[]}, 'A', 'C', None),
-        ({'A':['B', 'C'], 'B':['A', 'D'], 'C':['A', 'D'], 'D':['B', 'C', 'E'], 'E':['D']}, 'A', 'E', 3),
-        ({'A': ['B', 'C'], 'B':['D', 'F'], 'C':['D', 'E'], 'D':['E']}, 'A', 'A', 0),
+        ({'A': [], 'B': [], 'C': []}, 'A', 'C', None),
+        ({'A': ['B', 'C'], 'B': ['D'], 'C': ['D'], 'D': ['E'], 'E': []}, 'A', 'E', 3),
+        ({'A': ['B', 'C'], 'B': ['D', 'A'], 'C': ['D', 'E'], 'D': ['E']}, 'A', 'A', 0),
+        ({}, 'A', 'A', None)
     ]
 )
 def test(gr, st, cur, forward):
