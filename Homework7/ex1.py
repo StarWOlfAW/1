da62d05f8184c548101671be7ee610db312ed98f
sofiashedova <sofia.shedova@gmail.com>
2023-12-09 13:32:30 +0300

    condition

--- Homework7/ex1.py
+++ Homework7/ex1.py
@@ -5,14 +5,14 @@ def condition(this, cur):
     return this == cur
 
 
-def search(gr, st, cur):
+def search(gr, st, cur, f):
     if not gr:
         return None
     k = deque([(st, 0)])
     d = set([st])
     while k:
         this, forward = k.popleft()
-        if condition(this, cur):
+        if f(this, cur):
             return forward
         for near in gr.get(this, []):
             if near not in d:
--- Homework7/tests/test_1.py
+++ Homework7/tests/test_1.py
@@ -2,14 +2,23 @@ import pytest
 from Homework7.ex1 import search
 
 
+def condition(this, cur):
+    return this == cur
+
+
+def cc(this, cur):
+    return this[0] == 'A'
+
+
 @pytest.mark.parametrize(
-    ('gr', 'st', 'cur', 'forward'),
+    ('gr', 'st', 'cur', 'forward', 'f'),
     [
-        ({'A': [], 'B': [], 'C': []}, 'A', 'C', None),
-        ({'A': ['B', 'C'], 'B': ['D'], 'C': ['D'], 'D': ['E'], 'E': []}, 'A', 'E', 3),
-        ({'A': ['B', 'C'], 'B': ['D', 'A'], 'C': ['D', 'E'], 'D': ['E']}, 'A', 'A', 0),
-        ({}, 'A', 'A', None)
+        ({'A': [], 'B': [], 'C': []}, 'A', 'C', None, condition),
+        ({'A': ['B', 'C'], 'B': ['D'], 'C': ['D'], 'D': ['E'], 'E': []}, 'A', 'E', 3, condition),
+        ({'A': ['B', 'C'], 'B': ['D', 'A'], 'C': ['D', 'E'], 'D': ['E']}, 'A', 'A', 0, condition),
+        ({}, 'A', 'A', None, condition),
+        ({'B': ['A'], 'A': []}, 'B', 'A', 1, cc)
     ]
 )
-def test(gr, st, cur, forward):
-    assert search(gr, st, cur) == forward
+def test(gr, st, cur, forward, f):
+    assert search(gr, st, cur, f) == forward
