7f79ddd0cee0f60348554ae01cd3a64abfea76f1
sofiashedova <sofia.shedova@gmail.com>
2023-12-09 13:24:02 +0300

    condition

--- Homework7/ex1.py
+++ Homework7/ex1.py
@@ -1,6 +1,10 @@
 from collections import deque
 
 
+def condition(this, cur):
+    return this == cur
+
+
 def search(gr, st, cur):
     if not gr:
         return None
@@ -8,7 +12,7 @@ def search(gr, st, cur):
     d = set([st])
     while k:
         this, forward = k.popleft()
-        if this == cur:
+        if condition(this, cur):
             return forward
         for near in gr.get(this, []):
             if near not in d:
