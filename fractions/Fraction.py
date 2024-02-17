7a554e3dae51228adb6c41004b4db68d6b8676d4
StarWolfAW <evdryakin@gmail.com>
2024-02-17 11:14:25 +0300

    fff

---
+++ fractions/Fraction.py
@@ -0,0 +1,17 @@
+class Fraction:
+    def __init__(self, numer=1, denom=1):
+        self.numer = numer
+        self.denum = denom
+    def __str__(self):
+        return str(self.numer) + "/" + str(self.denom)
+    def input(self):
+        self.numer = int(input())
+        self.denom = int(input())
+        if self.denom == 0:
+            return "Не делить на ноль!"
+m1 = Fraction()
+m1.input()
+print(m1)
+
+
+
